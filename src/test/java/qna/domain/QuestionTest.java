e2dafa9d7533796e54ca7d3a3909a4445b376b71
jiokim <jiokim222@gmail.com>
2023-01-15 09:13:35 +0900

    test(QuestionTest): 테스트코드 수정

--- src/test/java/qna/domain/QuestionTest.java
+++ src/test/java/qna/domain/QuestionTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class QuestionTest {
+
     public static final Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
     public static final Question Q2 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
 
@@ -29,20 +30,17 @@ public class QuestionTest {
     @Test
     @DisplayName("삭제성공_질문자 답변자 일치")
     void delete_success_with_answer() throws CannotDeleteException {
-        Question Q1 = new Question("title2", "contents2").writeBy(UserTest.SANJIGI);
-        Q1.addAnswer(new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2"));
-        Q1.delete(UserTest.SANJIGI);
+        Q1.addAnswer(AnswerTest.A1);
+        Q1.delete(UserTest.JAVAJIGI);
         assertThat(Q1.isDeleted()).isTrue();
     }
 
     @Test
     @DisplayName("삭제실패_질문자 답변자 불일치")
     void delete_fail_with_answer() {
-        Question Q1 = new Question("title1", "contents1").writeBy(UserTest.JAVAJIGI);
-        Q1.addAnswer(new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2"));
-        Q1.addAnswer(new Answer(UserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2"));
+        Q2.addAnswer(AnswerTest.A1);
         assertThatThrownBy(() -> {
-            Q1.delete(UserTest.SANJIGI);
+            Q2.delete(UserTest.SANJIGI);
         }).isInstanceOf(CannotDeleteException.class);
     }
 }
